02692da80780c93e29a8cbbbcef2cbb4ff02ae11
Muzz109 <58022822+Muzz109@users.noreply.github.com>
2021-04-18 10:35:23 -0700

    commit test 1.0 in test class

--- demo/src/test/java/com/DrugStore/spring/DrugManagementSystemApplicationTests.java
+++ demo/src/test/java/com/DrugStore/spring/DrugManagementSystemApplicationTests.java
@@ -3,6 +3,7 @@ package com.DrugStore.spring;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+//change
 @SpringBootTest
 class DrugManagementSystemApplicationTests {
 
