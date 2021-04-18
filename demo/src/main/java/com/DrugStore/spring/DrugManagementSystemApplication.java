db5af73ee734d6e1ba23c0fc28a19c93dda4efc2
Muzz109 <58022822+Muzz109@users.noreply.github.com>
2021-04-18 09:52:31 -0700

    Initial commit

---
+++ demo/src/main/java/com/DrugStore/spring/DrugManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.DrugStore.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DrugManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DrugManagementSystemApplication.class, args);
+	}
+
+}
---
+++ demo/src/test/java/com/DrugStore/spring/DrugManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.DrugStore.spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DrugManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
